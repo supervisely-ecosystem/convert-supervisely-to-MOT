0b42bd8d5300a3ebabe811f3801e2788a2f205a7
Alex Emelyanov <alex84_84_84@mail.ru>
2021-07-19 15:19:24 +0300

    check functions

--- src/main.py
+++ src/main.py
@@ -34,100 +34,103 @@ else:
     DOWNLOAD_ALL_SHAPES = bool(util.strtobool(os.environ['modal.state.shapes']))
 
 
+def download_project(api, PROJECT_ID, dest_dir):
+    sly.download_video_project(api, PROJECT_ID, dest_dir, log_progress=True)
+
+
+def convert_project(dest_dir, RESULT_DIR, app_logger):
+    datasets_pathes = glob(dest_dir + "/*/")
+    if len(datasets_pathes) == 0:
+        logger.warn('There is no datasets in project')
+
+    meta_json = sly.io.json.load_json_file(os.path.join(dest_dir, 'meta.json'))
+    meta = sly.ProjectMeta.from_json(meta_json)
+
+    for ds_path in datasets_pathes:
+        ds_name = ds_path.split('/')[-2]
+        anns_pathes = glob(ds_path + "ann" + "/*")
+        progress = sly.Progress('Video being processed', len(anns_pathes), app_logger)
+        for ann_path in anns_pathes:
+            ann_json = sly.io.json.load_json_file(ann_path)
+            ann = sly.VideoAnnotation.from_json(ann_json, meta)
+            video_name = sly.io.fs.get_file_name(ann_path)
+            video_path = os.path.join(ds_path, "video", video_name)
+            video_info = sly.video.get_info(video_path)['streams'][0]
+
+            curr_objs_geometry_types = [obj.obj_class.geometry_type for obj in ann.objects]
+
+            if not DOWNLOAD_ALL_SHAPES and Rectangle not in curr_objs_geometry_types:
+                logger.warn('Video {} not contain firuges with shape Rectangle'.format(video_name))
+                continue
+
+            result_images = os.path.join(RESULT_DIR, ds_name, dir_train, get_file_name(video_name),
+                                         images_dir_name)
+            result_anns = os.path.join(RESULT_DIR, ds_name, dir_train, get_file_name(video_name),
+                                       ann_dir_name)
+            seq_path = os.path.join(RESULT_DIR, ds_name, dir_train, get_file_name(video_name), seq_name)
+
+            mkdir(result_images)
+            mkdir(result_anns)
+
+            with open(seq_path, 'a') as f:
+                f.write('[Sequence]\n')
+                f.write('name={}\n'.format(get_file_name(video_name)))
+                f.write('imDir={}\n'.format(images_dir_name))
+                f.write('frameRate={}\n'.format(round(1 / video_info['framesToTimecodes'][1])))
+                f.write('seqLength={}\n'.format(video_info['framesCount']))
+                f.write('imWidth={}\n'.format(video_info['width']))
+                f.write('imHeight={}\n'.format(video_info['height']))
+                f.write('imExt={}\n'.format(image_ext))
+
+            id_to_video_obj = {}
+            for idx, curr_video_obj in enumerate(ann.objects):
+                id_to_video_obj[curr_video_obj] = idx + 1
+
+            if len(ann.frames) == 0:
+                logger.warn('There is no figures in {} video, gt.txt file will be empty'.format(video_name))
+
+            for frame_index, frame in enumerate(ann.frames):
+                for figure in frame.figures:
+                    if not DOWNLOAD_ALL_SHAPES and figure.video_object.obj_class.geometry_type != Rectangle:
+                        continue
+
+                    rectangle_geom = figure.geometry.to_bbox()
+                    left = rectangle_geom.left
+                    top = rectangle_geom.top
+                    width = rectangle_geom.width
+                    height = rectangle_geom.height
+                    conf_val = 1
+                    for curr_tag in figure.video_object.tags:
+                        if conf_tag_name == curr_tag.name and (
+                                curr_tag.frame_range is None or frame_index in range(curr_tag.frame_range[0],
+                                                                                     curr_tag.frame_range[1] + 1)):
+                            conf_val = 0
+                    curr_gt_data = '{},{},{},{},{},{},{},{},{},{}\n'.format(frame_index + 1,
+                                                                            id_to_video_obj[figure.video_object],
+                                                                            left, top, width - 1, height - 1,
+                                                                            conf_val, -1, -1, -1)
+                    filename = 'gt_{}.txt'.format(figure.parent_object.obj_class.name)
+                    with open(os.path.join(result_anns, filename), 'a') as f:  # gt_path
+                        f.write(curr_gt_data)
+                if frame_index == ann.frames_count:
+                    break
+
+            vidcap = cv2.VideoCapture(video_path)
+            success, image = vidcap.read()
+            count = 1
+            while success:
+                image_name = str(count).zfill(6) + image_ext
+                image_path = os.path.join(result_images, image_name)
+                cv2.imwrite(image_path, image)
+                success, image = vidcap.read()
+                count += 1
+
+            progress.iter_done_report()
+
+
 @my_app.callback("from_sl_to_MOT")
 @sly.timeit
 def from_sl_to_MOT(api: sly.Api, task_id, context, state, app_logger):
-
-
-    def download_project(PROJECT_ID, dest_dir):
-        sly.download_video_project(api, PROJECT_ID, dest_dir, log_progress=True)
-
-
-    def convert_project(dest_dir, meta):
-        datasets_pathes = glob(dest_dir + "/*/")
-        if len(datasets_pathes) == 0:
-            logger.warn('There is no datasets in project {}'.format(project_name))
-
-        for ds_path in datasets_pathes:
-            ds_name = ds_path.split('/')[-2]
-            anns_pathes = glob(ds_path + "ann" + "/*")
-            progress = sly.Progress('Video being processed', len(anns_pathes), app_logger)
-            for ann_path in anns_pathes:
-                ann_json = sly.io.json.load_json_file(ann_path)
-                ann = sly.VideoAnnotation.from_json(ann_json, meta)
-                video_name = sly.io.fs.get_file_name(ann_path)
-                video_path = os.path.join(ds_path, "video", video_name)
-                video_info = sly.video.get_info(video_path)['streams'][0]
-
-                curr_objs_geometry_types = [obj.obj_class.geometry_type for obj in ann.objects]
-
-                if not DOWNLOAD_ALL_SHAPES and Rectangle not in curr_objs_geometry_types:
-                    logger.warn('Video {} not contain firuges with shape Rectangle'.format(video_name))
-                    continue
-
-                result_images = os.path.join(RESULT_DIR, ds_name, dir_train, get_file_name(video_name),
-                                             images_dir_name)
-                result_anns = os.path.join(RESULT_DIR, ds_name, dir_train, get_file_name(video_name),
-                                           ann_dir_name)
-                seq_path = os.path.join(RESULT_DIR, ds_name, dir_train, get_file_name(video_name), seq_name)
-
-                mkdir(result_images)
-                mkdir(result_anns)
-
-                with open(seq_path, 'a') as f:
-                    f.write('[Sequence]\n')
-                    f.write('name={}\n'.format(get_file_name(video_name)))
-                    f.write('imDir={}\n'.format(images_dir_name))
-                    f.write('frameRate={}\n'.format(round(1 / video_info['framesToTimecodes'][1])))
-                    f.write('seqLength={}\n'.format(video_info['framesCount']))
-                    f.write('imWidth={}\n'.format(video_info['width']))
-                    f.write('imHeight={}\n'.format(video_info['height']))
-                    f.write('imExt={}\n'.format(image_ext))
-
-                id_to_video_obj = {}
-                for idx, curr_video_obj in enumerate(ann.objects):
-                    id_to_video_obj[curr_video_obj] = idx + 1
-
-                if len(ann.frames) == 0:
-                    logger.warn('There is no figures in {} video, gt.txt file will be empty'.format(video_name))
-
-                for frame_index, frame in enumerate(ann.frames):
-                    for figure in frame.figures:
-                        if not DOWNLOAD_ALL_SHAPES and figure.video_object.obj_class.geometry_type != Rectangle:
-                            continue
-
-                        rectangle_geom = figure.geometry.to_bbox()
-                        left = rectangle_geom.left
-                        top = rectangle_geom.top
-                        width = rectangle_geom.width
-                        height = rectangle_geom.height
-                        conf_val = 1
-                        for curr_tag in figure.video_object.tags:
-                            if conf_tag_name == curr_tag.name and (
-                                    curr_tag.frame_range is None or frame_index in range(curr_tag.frame_range[0],
-                                                                                         curr_tag.frame_range[1] + 1)):
-                                conf_val = 0
-                        curr_gt_data = '{},{},{},{},{},{},{},{},{},{}\n'.format(frame_index + 1,
-                                                                                id_to_video_obj[figure.video_object],
-                                                                                left, top, width - 1, height - 1,
-                                                                                conf_val, -1, -1, -1)
-                        filename = 'gt_{}.txt'.format(figure.parent_object.obj_class.name)
-                        with open(os.path.join(result_anns, filename), 'a') as f:  # gt_path
-                            f.write(curr_gt_data)
-                    if frame_index == ann.frames_count:
-                        break
-
-                vidcap = cv2.VideoCapture(video_path)
-                success, image = vidcap.read()
-                count = 1
-                while success:
-                    image_name = str(count).zfill(6) + image_ext
-                    image_path = os.path.join(result_images, image_name)
-                    cv2.imwrite(image_path, image)
-                    success, image = vidcap.read()
-                    count += 1
-
-                progress.iter_done_report()
 
     project = api.project.get_info_by_id(PROJECT_ID)
     project_name = project.name
@@ -136,13 +139,13 @@ def from_sl_to_MOT(api: sly.Api, task_id, context, state, app_logger):
     meta = sly.ProjectMeta.from_json(meta_json)
 
     dest_dir = os.path.join(my_app.data_dir, working_folder)
-    download_project(PROJECT_ID, dest_dir)
+    download_project(api, PROJECT_ID, dest_dir)
 
     RESULT_ARCHIVE = os.path.join(my_app.data_dir, ARCHIVE_NAME)
     RESULT_DIR = os.path.join(my_app.data_dir, RESULT_DIR_NAME)
     mkdir(RESULT_DIR)
 
-    convert_project(dest_dir, meta)
+    convert_project(dest_dir, RESULT_DIR, app_logger)
 
     sly.fs.archive_directory(RESULT_DIR, RESULT_ARCHIVE)
     app_logger.info("Result directory is archived")
